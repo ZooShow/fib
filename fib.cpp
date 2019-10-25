f93e5182ce2ff5a18c533605b702c6305e1ece20
ZooShow <fedulzhuravlev@mail.ru>
2019-10-26 00:00:22 +1000

    make tests

---
+++ fib.cpp
@@ -0,0 +1,11 @@
+int fib (int a){
+    if (a<1){
+        std::cout << "Число должно быть > 0";
+        return 0;
+    }
+    if (a ==1 || a==2) {
+        return 1;
+    } else{
+        return fib(a-1)+fib(a-2);
+    }
+}
